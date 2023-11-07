e1f7040ec1ab12ed761474df30580bbe782541d4
Leander van Aarde <90837242+LeandervanAarde@users.noreply.github.com>
2023-11-07 13:40:52 +0200

    .

--- pages/readings.tsx
+++ pages/readings.tsx
@@ -108,7 +108,7 @@ export default function Readings() {
 
   useEffect(() => {
     groupedData();
-  }, [dat]);
+  }, [groupedData, dat]);
 
   const validateToken = async (token: string) => {
     try {
